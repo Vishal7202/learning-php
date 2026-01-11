bb0b5440d8e39233f22672be986a42b923bf93b7
Vishal Kumar <vishalkumar7295997202@gmail.com>
2026-01-11 11:11:30 +0530

    Add PHP variables example

---
+++ variables.php
@@ -0,0 +1,11 @@
+<?php
+// PHP Variables Example
+
+$name = "Vishal";
+$age = 22;
+$isStudent = true;
+
+echo "Name: " . $name . "<br>";
+echo "Age: " . $age . "<br>";
+echo "Student: " . $isStudent;
+?>
